a8e103571083b14ee5da21702bf2e981d27b2359
yisus82 <djalma8@gmail.com>
2022-03-11 13:07:33 +0100

    Add font & draw texts

--- main.py
+++ main.py
@@ -14,29 +14,36 @@ pygame.display.set_caption("Space Invaders")
 # region Load images
 # Enemy spaceships
 BLUE_SPACESHIP = pygame.image.load(
-    os.path.join("assets", "pixel_ship_blue_small.png"))
+    os.path.join("assets/images", "pixel_ship_blue_small.png"))
 GREEN_SPACESHIP = pygame.image.load(
-    os.path.join("assets", "pixel_ship_green_small.png"))
+    os.path.join("assets/images", "pixel_ship_green_small.png"))
 RED_SPACESHIP = pygame.image.load(
-    os.path.join("assets", "pixel_ship_red_small.png"))
+    os.path.join("assets/images", "pixel_ship_red_small.png"))
 
 # Player spaceship
 YELLOW_SPACESHIP = pygame.image.load(
-    os.path.join("assets", "pixel_ship_yellow.png"))
+    os.path.join("assets/images", "pixel_ship_yellow.png"))
 
 # Enemy lasers
-BLUE_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_blue.png"))
+BLUE_LASER = pygame.image.load(os.path.join(
+    "assets/images", "pixel_laser_blue.png"))
 GREEN_LASER = pygame.image.load(
-    os.path.join("assets", "pixel_laser_green.png"))
-RED_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_red.png"))
+    os.path.join("assets/images", "pixel_laser_green.png"))
+RED_LASER = pygame.image.load(os.path.join(
+    "assets/images", "pixel_laser_red.png"))
 
 # Player laser
 YELLOW_LASER = pygame.image.load(
-    os.path.join("assets", "pixel_laser_yellow.png"))
+    os.path.join("assets/images", "pixel_laser_yellow.png"))
 
 # Background
 BACKGROUND = pygame.transform.scale(pygame.image.load(
-    os.path.join("assets", "background_black.png")), (WIDTH, HEIGHT))
+    os.path.join("assets/images", "background_black.png")), (WIDTH, HEIGHT))
+# endregion
+
+# region Load fonts
+pygame.font.init()
+MAIN_FONT = pygame.font.Font(os.path.join("assets/fonts", "prstart.ttf"), 20)
 # endregion
 
 
@@ -44,11 +51,21 @@ def main():
     """Main function"""
     run = True
     FPS = 60
+    level = 1
+    lives = 5
     clock = pygame.time.Clock()
 
     def redraw_window():
         """Redraw the window"""
+        # Background
         WINDOW.blit(BACKGROUND, (0, 0))
+
+        # Draw texts
+        lives_label = MAIN_FONT.render(f"Lives: {lives}", 1, (255, 255, 255))
+        level_label = MAIN_FONT.render(f"Level: {level}", 1, (255, 255, 255))
+        WINDOW.blit(lives_label, (10, 10))
+        WINDOW.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
+
         pygame.display.update()
 
     while run:
